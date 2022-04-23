9185806bf9886cbf400d24efad3e77009d8df41f
CodingFlops <68713163+CodingFlops@users.noreply.github.com>
2022-04-23 15:43:07 -0700

    updated

--- MakeOutWord.py
+++ MakeOutWord.py
@@ -1,5 +1,4 @@
 fourletterword = input("Type a something that is 4 letters long ")
-wordletter4 = fourletterword
 words = input("Type a word ")
-something = fourletterword[:2] + words + wordletter4[2:]
+something = fourletterword[:2] + words + fourletterword[2:]
 print(something)
