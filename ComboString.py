7ef66827d5d6b19f1ba86361cb3706621a112ef0
CodingFlops <68713163+CodingFlops@users.noreply.github.com>
2022-04-30 15:56:45 -0700

    New and improved string combo

--- ComboString.py
+++ ComboString.py
@@ -2,5 +2,11 @@ word = input("Write a word ")
 word2 = input("Type in a word ")
 if len(word) > len(word2):
     print("your combo string is (drumroll please)... " + word2 + word + word2)
-else:
+    word2 + word + word2
+elif len(word) < len(word2):
     print("your combo string is (drumroll please)... " + word + word2 + word)
+    later = word + word2 + word
+else:
+    print("ERROR")
+    print("VARIBALES SAME SIZE")
+print(later)
