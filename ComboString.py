483de11599854f6f6e49e39fa84b65d673a87cd0
CodingFlops <68713163+CodingFlops@users.noreply.github.com>
2022-04-30 15:20:53 -0700

    A new program called ComboString.py

---
+++ ComboString.py
@@ -0,0 +1,3 @@
+short = input("Write a word (It is recommened to write a short word) ")
+long = input("Type in a word that is longer than the last word you typed ")
+print("your combo string is (drumroll please)... " + short + long + short)
