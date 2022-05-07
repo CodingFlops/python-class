1d0ec96c1b69eafa3122d4094a728765fde7eb0a
CodingFlops <68713163+CodingFlops@users.noreply.github.com>
2022-05-07 16:28:06 -0700

    improved replace a letter

--- ReplaceALetter.py
+++ ReplaceALetter.py
@@ -1,9 +1,5 @@
 word = input("Write a word ")
 number = int(input("Write a number "))
 def replace(string, index):
-    stringList = list(string)
-    stringList.pop(index - 1)
-    stringList.insert(index - 1, "-")
-    newString = "".join(stringList)
-    return newString
-print(replace(word, number))
+    print(string[:index - 1] + "-" + string[index:])
+replace(word, number)
