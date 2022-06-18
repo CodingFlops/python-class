e4c64944875518c9830863ec90a0286724e01ec4
CodingFlops <68713163+CodingFlops@users.noreply.github.com>
2022-06-18 16:40:41 -0700

    newest program

---
+++ lanty.py
@@ -0,0 +1,15 @@
+xedni = input("Type a word ")
+lant = input("Now choose your direction like this: \ (Type r) or this | (Type d) or this / (Type l) ").lower()
+index = 0
+space = ""
+if lant == "l":
+    for i in range (len(xedni)):
+        space = space + " "
+print("Your word is " + xedni)
+for i in range(len(xedni)):
+    if lant == "r":
+        space = space + " "
+    elif lant == "l":
+        space = space[:-1]
+    print(space + xedni[index] + "!")
+    index = index + 1
